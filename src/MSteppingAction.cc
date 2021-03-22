c5a9345148a3a284d8b22ec89cfde3065577c754
pietrobisio <pietrobisio@MacBook-Air-di-Pietro.local>
2021-03-22 15:26:50 +0100

    solved MAC error

--- src/MSteppingAction.cc
+++ src/MSteppingAction.cc
@@ -73,7 +73,7 @@ void MSteppingAction::UserSteppingAction(const G4Step *aStep) {
 		if (aStep->GetPreStepPoint()->GetPhysicalVolume() != aStep->GetPostStepPoint()->GetPhysicalVolume()) {
 			if (aStep->GetPreStepPoint()->GetSensitiveDetector() != 0) {
 				G4VSensitiveDetector *SD = aStep->GetPreStepPoint()->GetSensitiveDetector();
-				if (SD->GetName() == evt_action->SDprompt_2) {
+				if (((string)SD->GetName()) == evt_action->SDprompt_2) {
 					if ((aStep->GetPreStepPoint()->GetGlobalTime() >= evt_action->Tprompt_MIN) && (aStep->GetPreStepPoint()->GetGlobalTime() <= evt_action->Tprompt_MAX) && (track->GetParentID()!=0)) {
 						evt_action->Eprompt_2 += track->GetKineticEnergy();
 					}
